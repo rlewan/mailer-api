0c986739908099a75416f76ae617aaf834a6ce70
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-01-24 21:15:58 +0000

    Document response codes

--- src/main/java/com/github/rlewan/mailer/controllers/EmailsController.java
+++ src/main/java/com/github/rlewan/mailer/controllers/EmailsController.java
@@ -4,6 +4,9 @@ import com.github.rlewan.mailer.services.EmailSender;
 import com.github.rlewan.mailer.model.SendEmailRequest;
 import com.github.rlewan.mailer.model.SendEmailResponse;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -31,6 +34,13 @@ public class EmailsController {
     }
 
     @PostMapping
+    @ApiOperation("Dispatches an email message to one of downstream providers")
+    @ApiResponses({
+        @ApiResponse(code = 202, message = "Your request has been dispatched for sending"),
+        @ApiResponse(code = 400, message = "Your request was not well formed (perhaps body was missing?)"),
+        @ApiResponse(code = 422, message = "The request was well formed, but did not pass validation"),
+        @ApiResponse(code = 503, message = "No downstream providers are available to handle your request at this moment")
+    })
     public ResponseEntity<SendEmailResponse> sendEmail(@Valid @RequestBody SendEmailRequest request) {
         SendEmailResponse response = emailSender.sendEmail(request);
         return ResponseEntity
