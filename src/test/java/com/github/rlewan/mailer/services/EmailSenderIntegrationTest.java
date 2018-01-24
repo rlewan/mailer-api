36861bc1f7b7e1a544674b29cd192b3c29d2616a
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-01-24 18:56:07 +0000

    Provide EmailSender integration tests

---
+++ src/test/java/com/github/rlewan/mailer/services/EmailSenderIntegrationTest.java
@@ -0,0 +1,94 @@
+package com.github.rlewan.mailer.services;
+
+import com.github.rlewan.mailer.exceptions.ServiceUnavailableException;
+import com.github.rlewan.mailer.model.SendEmailRequest;
+import com.github.rlewan.mailer.model.SendEmailResponse;
+import com.github.rlewan.mailer.services.providers.EmailServiceProvider;
+import com.github.rlewan.mailer.services.providers.EmailServiceProviderException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmailSenderIntegrationTest {
+
+    @MockBean
+    @Qualifier("primaryEmailServiceProvider")
+    private EmailServiceProvider primaryEmailServiceProvider;
+
+    @MockBean
+    @Qualifier("secondaryEmailServiceProvider")
+    private EmailServiceProvider secondaryEmailServiceProvider;
+
+    @Mock
+    private SendEmailRequest request;
+
+    @Autowired
+    private EmailSender emailSender;
+
+    @Before
+    public void beforeEachTest() {
+        when(request.getSubject()).thenReturn(Optional.empty());
+        when(request.getContent()).thenReturn(Optional.empty());
+    }
+
+    @Test
+    public void shouldReturnAcceptedResponseWhenPrimaryProviderSucceeds() {
+        mockProviderResponseCode(primaryEmailServiceProvider, 200);
+
+        SendEmailResponse response = emailSender.sendEmail(request);
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.ACCEPTED.value());
+    }
+
+    private void mockProviderResponseCode(EmailServiceProvider provider, int responseCode) {
+        when(provider.sendEmail(anyString(), anyString(), anyString(), anyString()))
+            .thenReturn(responseCode);
+    }
+
+    @Test
+    public void shouldReturnAcceptedResponseWhenFallbackToSecondaryProviderSucceeds() {
+        mockProviderToThrow(primaryEmailServiceProvider);
+        mockProviderResponseCode(secondaryEmailServiceProvider, 200);
+
+        SendEmailResponse response = emailSender.sendEmail(request);
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.ACCEPTED.value());
+    }
+
+    private void mockProviderToThrow(EmailServiceProvider provider) {
+        when(provider.sendEmail(anyString(), anyString(), anyString(), anyString()))
+            .thenThrow(new RuntimeException("Something went wrong!"));
+    }
+
+    @Test(expected = ServiceUnavailableException.class)
+    public void shouldThrowServiceUnavailableWhenBothProvidersFailWithException() {
+        mockProviderToThrow(primaryEmailServiceProvider);
+        mockProviderToThrow(secondaryEmailServiceProvider);
+
+        emailSender.sendEmail(request);
+    }
+
+    @Test(expected = ServiceUnavailableException.class)
+    public void shouldThrowServiceUnavailableWhenBothProvidersReturnFailureResponseCode() {
+        mockProviderResponseCode(primaryEmailServiceProvider, 500);
+        mockProviderResponseCode(secondaryEmailServiceProvider, 500);
+
+        emailSender.sendEmail(request);
+    }
+
+}
