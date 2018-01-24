e75bfff275245edd8ad8670fa83cfea355194760
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-01-24 01:13:28 +0000

    Add a 503 response description

--- src/main/java/com/github/rlewan/mailer/exceptions/ServiceUnavailableException.java
+++ src/main/java/com/github/rlewan/mailer/exceptions/ServiceUnavailableException.java
@@ -3,6 +3,9 @@ package com.github.rlewan.mailer.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.SERVICE_UNAVAILABLE)
+@ResponseStatus(
+    code = HttpStatus.SERVICE_UNAVAILABLE,
+    reason = "Downstream services are not available at the moment"
+)
 public class ServiceUnavailableException extends RuntimeException {
 }
